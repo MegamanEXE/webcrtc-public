bd342ef79afbcd8c236bffe8b62ca38be144fdff
Hamza Masud <MegamanEXE@live.com>
2023-02-27 04:54:20 +0100

    click listener bug fixed

--- src/Components/CreateTest/Toolbox.js
+++ src/Components/CreateTest/Toolbox.js
@@ -103,14 +103,8 @@ export default function Toolbox(props) {
   }
 
   const handleClick = (event) => {
-    // console.log(event.target)
-    let type;
-     
-    //Because somehow the Icon and the Button base are separate entities in MUI's toggle button
-    if (event.target.attributes.shape === undefined) 
-      type = event.target.parentElement.attributes.shape.value;
-    else
-      type = event.target.attributes.shape.value;
+    // console.log(event.currentTarget)
+    let type = event.currentTarget.attributes.shape.value;
 
     const matrix_size = 150; //Match this up with matrix_size in Matrix.js. Not needed generally.
     const randPadding = matrix_size/4;
